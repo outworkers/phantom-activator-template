48f26614ca651e270cab0063515fdc0495407a12
Flavian Alexandru <flavian.alexandru@anomaly42.com>
2016-05-20 16:09:16 +0100

    Adding an SBT plugin dependency.

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -7,11 +7,11 @@ import scala.concurrent.ExecutionContext.Implicits.global
 
 object Application extends Controller with AppDatabaseProvider {
 
-  def index = Action {
+  def index: Action[AnyContent] = Action {
     Ok(views.html.index("Your new application is ready."))
   }
 
-  def beers(style: String) = Action.async {
+  def beers(style: String): Action[AnyContent] = Action.async {
     database.beers.getByStyle(style).map { beers =>
       Ok(views.html.beers(style, beers))
     }
